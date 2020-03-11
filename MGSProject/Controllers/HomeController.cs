92cf3a277a9affa072b01cbc27cdb91fd6600d1b
or mizrahi <ormizrahi777@gmail.com>
2020-03-11 17:07:57 +0200

    changed SaveRepos to non async method

--- MGSProject/Controllers/HomeController.cs
+++ MGSProject/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -16,44 +17,43 @@ namespace MGSProject.Controllers
     public class HomeController : Controller
     {
         private readonly MainDbContext _dbContext = new MainDbContext();
-        private static readonly HttpClient Client = new HttpClient();
-
-
+        
         public ActionResult Index()
         {
             return View();
         }
 
 
-        public async Task<string> SaveRepos(string repoName)
+        [HttpPost]
+        public ActionResult SaveRepos(string repoName)
         {
-            Client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("MGSProject", "1"));
-
-            var response = await Client.GetAsync($"https://api.github.com/search/repositories?q={repoName}");
-            if (!response.IsSuccessStatusCode) return null;
-            var result = await response.Content.ReadAsAsync<RootObject>();
-            var json = await response.Content.ReadAsStringAsync();
-
-            foreach (var repo in result.items)
+            using (var webClient = new WebClient())
             {
-                var currentHtmlUrl = repo.html_url;
-                var repository = _dbContext.Repositories.SingleOrDefault(el => el.HtmlUrl == currentHtmlUrl);
-                if (repository == null)
+                webClient.Headers["User-Agent"] = Request.Headers["User-Agent"];
+                var json = webClient.DownloadString($"https://api.github.com/search/repositories?q={repoName}");
+                var result = JsonConvert.DeserializeObject<RootObject>(json);
+
+                foreach (var repo in result.items)
                 {
-                    repository = new Repository
+                    var currentHtmlUrl = repo.html_url;
+                    var repository = _dbContext.Repositories.SingleOrDefault(el => el.HtmlUrl == currentHtmlUrl);
+                    if (repository == null)
                     {
-                        HtmlUrl = currentHtmlUrl,
-                        NumberOfAppearances = 0
-                    };
-                    _dbContext.Repositories.Add(repository);
+                        repository = new Repository
+                        {
+                            HtmlUrl = currentHtmlUrl,
+                            NumberOfAppearances = 0
+                        };
+                        _dbContext.Repositories.Add(repository);
+                    }
+
+                    repository.NumberOfAppearances++;
                 }
 
-                repository.NumberOfAppearances++;
+                _dbContext.SaveChanges();
+
+                return Json(json);
             }
-
-            _dbContext.SaveChanges();
-
-            return json;
         }
     }
 }
